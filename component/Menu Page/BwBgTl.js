ad9651a183632b0b70baf718cb0fb25efe817819
Adwait Gaikwad <68456350+Mojo-cli@users.noreply.github.com>
2020-08-21 10:53:27 +0530

    Add files via upload

---
+++ component/Menu Page/BwBgTl.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+
+const BwBgTl = props => {
+  const content = (
+    <View style={[styles.button, { backgroundColor: props.color }]}>
+    <Text style={styles.text}>{props.text}</Text>
+    </View>
+  )
+return <TouchableOpacity onPress={props.onPress}>{content}</TouchableOpacity>
+}
+
+const styles = StyleSheet.create({
+  button:{
+     marginTop:25,
+     padding:10,
+     width:300,
+     borderRadius:10
+  },
+  text:{
+  color:"white",
+  textAlign:"center",
+  fontSize:15
+  }
+});
+
+export default BwBgTl;
